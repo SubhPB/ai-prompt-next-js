aa04d8b8a8ed76154b3e280f2689e1ed21263659
subh <harvirbuttar2917@gmail.com>
2024-05-11 10:25:32 -0700

    ...

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -2,12 +2,13 @@
 
 "use client";
 
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-export default function AuthProvider({children}: {children: React.ReactNode}){
+export default function AuthProvider({children, session=null}: {children: React.ReactNode, session?: Session | null}){
 
     return (
-        <SessionProvider>
+        <SessionProvider session={session}>
             {children}
         </SessionProvider>
     )
